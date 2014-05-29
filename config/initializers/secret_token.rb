929ba52673314bf227ee68c90bb998f9bec8fd72
Nando Sousa <nandosousafr@gmail.com>
2014-05-28 21:20:08 -0300

    Initial commit.
    Generated by pah version 0.0.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devinsanta::Application.config.secret_key_base = '624687d8d9249d3e8544346de15ce2be865f470a5c466b64b3549cbb5d4a2dc9a1dcc2f47ba7f28f73176b02616f27c8f3bdc71db8b578a567403752cbf81eb7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Devinsanta::Application.config.session_store :cookie_store, key: '_devinsanta_session'
